925549fa9ac015c5ddf50f637d5f94c0dda55c51
Milan Stanojlovic <mistikone1@gmail.com>
2019-01-11 19:31:11 +0100

    building search view

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+// All DOM elements 
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResultList: document.querySelector('.results__list'),
+    
+};
