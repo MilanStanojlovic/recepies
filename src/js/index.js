34f66c83ea460db2783f362f0b417986677b6ed8
Milan Stanojlovic <mistikone1@gmail.com>
2019-01-19 22:51:56 +0100

    added list controller function

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -107,6 +108,19 @@ const controlRecipe = async () => {
 //Array that loops over strings(event types) and calls eventListener
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+
+// LIST CONTROLLER
+const controlList = () => {
+    //1.Create  a new list IF there is none 
+    if(!state.list) state.list = new List();
+
+    //2. Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+};
+
 //Handling recipe button clicks
 elements.recipe.addEventListener('click', e=>{
     if(e.target.matches('.btn-decrease, .btn-decrease * ')){
@@ -119,6 +133,8 @@ elements.recipe.addEventListener('click', e=>{
         //Increase button is clickded
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    }else if(e.target.matches('.recipe__btn--add, .recipe__btn * ')){
+        controlList();
     }
 
     // console.log(state.recipe);
